de0ab3adcfba7056454e36202bf13c47126d5b6a
Anuroop <anuroopkr13@gmail.com>
2024-07-12 14:25:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-red-600 '>
+      <h1 className='size-6xl'>netflix</h1>
+    </div>
+  )}
+export default App
